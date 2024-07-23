4419728ac3a45fdda9da5e1922d227694a91655b
Rayfa79 <96704831+Rayfa79@users.noreply.github.com>
2024-07-23 19:05:01 -0400

    39- refactored to Input whole object istead of individual properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,19 @@
 import { Component,Input,Output,computed,input,output,signal } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
+//Use interface or type alias for object types
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
-
+//optional alias type
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
 
 
 
@@ -15,9 +26,11 @@ import { EventEmitter } from '@angular/core';
 })
 export class UserComponent {
   //allow property value to be settable from outside
-  @Input({required: true}) avatar!: string
-  @Input({required: true}) name!: string
-  @Input({required: true}) id!: string
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
+  // @Input({required: true}) id!: string
+  //refactor the above Inputs to use Object instead of individual properties
+  @Input({required: true}) users!: User;
 
   //create custom eventEmitter that emits data upon button click
   @Output() select = new EventEmitter<string>()
@@ -32,11 +45,11 @@ export class UserComponent {
 //   return 'assets/users/' + this.avatar()
 //  })
  get imagePath() {
-  return 'assets/users/' + this.avatar
+  return 'assets/users/' + this.users.avatar
  }
 
  //emit data from custom event to parent component
   onSelectedUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.users.id)
   }
 }
